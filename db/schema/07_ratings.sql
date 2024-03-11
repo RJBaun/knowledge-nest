70f793d5b009d5c48d44eae687a81e6388c9071a
Rylan Baun <rylanjbaun@gmail.com>
2024-03-11 14:54:57 -0600

    Add ratings schema and seed data

---
+++ db/schema/07_ratings.sql
@@ -0,0 +1,15 @@
+-- Created by Rylan Baun
+-- Created on March 11th, 2024
+-- Purpose: DB table to store information about different ratings
+
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  rater_id INTEGER REFERENCES users(id) NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) NOT NULL,
+  rating SMALLINT NOT NULL,
+  date TIMESTAMP NOT NULL
+);
+
+
