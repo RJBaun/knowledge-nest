bd5df5f71ae69745703ef6b5a5955da4e588f1d2
v-lane <victoria.lane.m@gmail.com>
2024-03-11 17:28:22 -0400

    Add resources queries

---
+++ db/queries/resources.js
@@ -0,0 +1,20 @@
+// Created by Victoria Lane
+// Created on March 11, 2024
+// Purpose: Resource-related queries for JS routes.
+
+const db = require('../connection');
+
+/*
+Required Queries
+ - Show all resources
+ - Given all resource fields, add new resource in resources
+ - Given id, show a specific resource
+ - Given all resource fields, update resource in resources
+ - Update resource field 'is_archived' to true (archive resource)
+*/
+
+
+
+
+
+module.exports = {  };
