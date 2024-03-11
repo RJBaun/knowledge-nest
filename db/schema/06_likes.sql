d2859e02b70a26828e189780c65ca00d38939d89
v-lane <victoria.lane.m@gmail.com>
2024-03-11 16:17:54 -0400

    Add likes table

---
+++ db/schema/06_likes.sql
@@ -0,0 +1,13 @@
+-- Drop and recreate resources table
+DROP TABLE IF EXISTS resources CASCADE;
+
+-- Created by Victoria Lane
+-- Created on March 11, 2024
+-- Purpose: DB table to store likes data
+
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  liker_id INTEGER REFERENCES users(id),
+  resource_id INTEGER REFERENCES resources(id),
+  date TIMESTAMP
+);
