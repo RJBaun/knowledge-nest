161d796957e176a3ec9dd59d528dea69c676735c
v-lane <victoria.lane.m@gmail.com>
2024-03-11 13:42:55 -0400

    Update table and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Created by Victoria Lane
+-- Created on March 11, 2024
+-- Purpose: DB table to store user data
+
+INSERT INTO users (username, email, password, date_created)
+VALUES
+
+('Alice', 'alice@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '2004-10-19 10:23:54'),
+('Kira', 'kira@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '2004-11-19 10:23:54');
