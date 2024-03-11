93eef800d48ec2b3e857fb21f6807d13216ca266
Rylan Baun <rylanjbaun@gmail.com>
2024-03-11 15:32:44 -0600

    Add createNewUser query

--- db/queries/users.js
+++ db/queries/users.js
@@ -12,6 +12,14 @@ getUserByID = (userID) => {
     .then(data => {
       return data.rows[0]
     });
-}
+};
+
+createNewUser = (userObj) => {
+  const { username, email, password, date_created } = userObj;
+  return db.query('INSERT INTO users (username, email, password, date_created) VALUES ($1, $2, $3, $4);', [username, email, password, date_created])
+    .then(data => {
+      return data.rows[0]
+    });
+};
 
 module.exports = { getUsers };
