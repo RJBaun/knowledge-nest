e2dad7ae2c5b5b1ade5247ab7a36360165270300
v-lane <victoria.lane.m@gmail.com>
2024-03-11 14:23:44 -0400

    Add resources table

---
+++ db/schema/04_resources.sql
@@ -0,0 +1,18 @@
+-- Drop and recreate resources table
+DROP TABLE IF EXISTS resources CASCADE;
+
+-- Created by Victoria Lane
+-- Created on March 11, 2024
+-- Purpose: DB table to store user data
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  url TEXT NOT NULL,
+  description VARCHAR(255)
+  owner_id INTEGER REFERENCES users(id),
+  category_id INTEGER REFERENCES categories(id),
+  resource_type_id INTEGER REFERENCES resource_types(id),
+  date_added TIMESTAMP,
+  is_archived BOOLEAN DEFAULT false
+);
