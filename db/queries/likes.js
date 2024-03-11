a20d3d9435fb96c259999629678e4b6e4da3d4e0
v-lane <victoria.lane.m@gmail.com>
2024-03-11 19:47:22 -0400

    Add like queries

---
+++ db/queries/likes.js
@@ -0,0 +1,39 @@
+// Created by Victoria Lane
+// Created on March 11, 2024
+// Purpose: Resource-related queries for JS routes.
+
+const db = require('../connection');
+
+/**
+ * Add like to database.
+ * @param {string} liker_id ID of user who liked the resource.
+ * @param {string} resource_id ID of resource that was liked.
+ * @returns {Promise<{}>} Promise to the like.
+ */
+const addLike = (liker_id, resource_id) => {
+  return db
+    .query(`INSERT INTO likes (liker_id, resource_id)
+  VALUES (${liker_id}, ${resource_id}) RETURNING *;`)
+    .then(data => {
+      return data.rows[0];
+    });
+};
+
+/**
+ * Count likes for specific resource.
+ * @param {string} resource_id ID of specific resource.
+ * @returns {Promise<{}>} Promise to the like.
+ */
+const countLikes = (resource_id) => {
+  return db
+  .query(`SELECT count(likes.*) FROM LIKES
+  RIGHT OUTER JOIN resources ON resource_id = resources.id
+  WHERE resources.id = ${resource_id}
+  GROUP BY resources.id;
+  `)
+  .then(data => {
+    return data.rows[0]
+  })
+}
+
+module.exports = { addLike, countLikes }
