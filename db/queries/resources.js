b5c12e99c1e2f9e7940c5836d046bb9230eef207
v-lane <victoria.lane.m@gmail.com>
2024-03-11 17:58:29 -0400

    Add addNewResource query

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -14,27 +14,54 @@ Required Queries
 */
 
 /**
- * Get all resources data
- * @returns Promise which returns array of object of all resources data
+ * Get all resources data.
+ * @returns {Promise<[{}]>} Promise to resources.
  */
 const getResources = () => {
-  return db.query('SELECT * FROM resources;')
-  .then(data => {
-    return data.rows;
-  });
-}
+  return db
+    .query('SELECT * FROM resources;')
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+      return null;
+    });
+};
 
 /**
- * Get all resources data
- * @returns Promise which returns object of single row from resources table
+ * Get resources data
+ * @param {string} id The id of the user.
+ * @returns {Promise<{}>} Promise to the resource.
  */
 const getResourceById = (id) => {
-  return db.query('SELECT * FROM resources WHERE id=$1;', [id])
-  .then(data => {
-    return data.rows[0];
-  });
-}
+  return db
+    .query('SELECT * FROM resources WHERE id=$1;', [id])
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(err => {
+      return null;  // user does not exist
+    });
+};
+
+/**
+ * Add new resource to the database.
+ * @param {{name: string, url: string, description: string, owner_id: string, category_id: string, resource_type_id: string, date_added: string}} resource
+ * @returns {Promise<{}>} A promise to the resource.
+ */
+const addNewResource = (resource) => {
+  return db
+  .query(`INSERT INTO resources (name, url, description, owner_id, category_id, resource_type_id, date_added)
+  )
+  VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [resource.name, resource.url, resource.description, resource.owner_id, resource.category_id, resource.resource_type_id, resource.date_added])
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(err => {
+      return null;
+    })
+};
 
 
-
-module.exports = { getResources };
+module.exports = { getResources, getResourceById, addNewResource };
