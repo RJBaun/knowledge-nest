16ce25912b50c0b2bb11c4586a2a3b3b0a54361f
Rylan Baun <rylanjbaun@gmail.com>
2024-03-11 17:23:02 -0600

    add RETURNING * to insert and update queries, correct comment notation returns, update getResourcesByUser into owned and liked quieries

--- db/queries/users.js
+++ db/queries/users.js
@@ -18,23 +18,26 @@ const getUsers = () => {
 /**
  * Get specific user data.
  * @param {string} userID
- * @returns {Promise<[{}]>} Promise to users.
+ * @returns {Promise<{}>} Promise to users.
  */
 const getUserByID = (userID) => {
   return db.query('SELECT * FROM users WHERE id = $1;', [userID])
     .then(data => {
       return data.rows[0]
-    });
+    })
+    .catch(err => {
+      return null;
+    })
 };
 
 /**
  * Insert new user to the database.
  * @param {{username:string, email:string, password:string, date_created:string}} userObj
- * @returns {Promise<[{}]>} Promise to users.
+ * @returns {Promise<{}>} Promise to users.
  */
 const createNewUser = (userObj) => {
   const { username, email, password, date_created } = userObj;
-  return db.query('INSERT INTO users (username, email, password, date_created) VALUES ($1, $2, $3, $4);', [username, email, password, date_created])
+  return db.query('INSERT INTO users (username, email, password, date_created) VALUES ($1, $2, $3, $4) RETURNING *;', [username, email, password, date_created])
     .then(data => {
       return data.rows[0]
     });
@@ -43,11 +46,11 @@ const createNewUser = (userObj) => {
 /**
  * Update a users profile in the database.
  * @param {{id:string, username:string, email:string}} userObj
- * @returns {Promise<[{}]>} Promise to users.
+ * @returns {Promise<{}>} Promise to users.
  */
 const editUserProfile = (userObj) => {
   const { id, username, email } = userObj;
-  return db.query('UPDATE users SET username = $2, email = $2 WHERE id = $1;', [id, username, email])
+  return db.query('UPDATE users SET username = $2, email = $3 WHERE id = $1 RETURNING *;', [id, username, email])
     .then(data => {
       return data.rows[0]
     });
@@ -56,16 +59,16 @@ const editUserProfile = (userObj) => {
 /**
  * Delete a user from the database and archive all owned resources.
  * @param {{id:string}} userObj
- * @returns {Promise<[{}]>} Promise to users.
+ * @returns {Promise<{}>} Promise to users.
  */
 const deleteUser = (userObj) => {
   const { id } = userObj;
-  return db.query('UPDATE users SET is_deleted = true WHERE id = $1;', [id])
+  return db.query('UPDATE users SET is_deleted = true WHERE id = $1 RETURNING *;', [id])
     .then(data => {
       return data.rows[0]
     })
     .then(() => {
-      return db.query('UPDATE resources SET is_archived = true WHERE owner_id = $1;', [id])
+      return db.query('UPDATE resources SET is_archived = true WHERE owner_id = $1 RETURNING *;', [id])
     });
 };;
 
@@ -74,12 +77,25 @@ const deleteUser = (userObj) => {
  * @param {{id:string}} userObj
  * @returns {Promise<[{}]>} Promise to users.
  */
-const getResourcesByUsers = (userObj) => {
+const getOwnedResources = (userObj) => {
   const { id } = userObj;
-  return db.query('SELECT * FROM resources WHERE owner_id = $1 UNION SELECT resources.* FROM resources JOIN likes ON resources.id = likes.resource_id WHERE liker_id = $1;', [id])
+  return db.query('SELECT * FROM resources WHERE owner_id = $1;', [id])
     .then(data => {
       return data.rows
     });
 };
 
-module.exports = { getUsers, getUserByID, createNewUser, editUserProfile, deleteUser, getResourcesByUsers };
+/**
+ * Get all resources associated with a user in the database.
+ * @param {{id:string}} userObj
+ * @returns {Promise<[{}]>} Promise to users.
+ */
+const getLikedResources = (userObj) => {
+  const { id } = userObj;
+  return db.query('SELECT resources.* FROM resources JOIN likes ON resources.id = likes.resource_id WHERE likes.liker_id = $1;', [id])
+    .then(data => {
+      return data.rows
+    });
+};
+
+module.exports = { getUsers, getUserByID, createNewUser, editUserProfile, deleteUser, getOwnedResources, getLikedResources };
