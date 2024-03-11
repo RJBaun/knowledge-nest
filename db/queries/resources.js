8881acd88a728a3b6d14a31f809e6682acc8d021
v-lane <victoria.lane.m@gmail.com>
2024-03-11 17:39:05 -0400

    Add getResources query

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -6,15 +6,26 @@ const db = require('../connection');
 
 /*
 Required Queries
- - Show all resources
+ - X Show all resources
+ - X Given id, show a specific resource
  - Given all resource fields, add new resource in resources
- - Given id, show a specific resource
  - Given all resource fields, update resource in resources
  - Update resource field 'is_archived' to true (archive resource)
 */
 
+/**
+ * Get all resources data
+ * @returns Promise which returns array of object of all resources data
+ */
+const getResources = () => {
+  return db.query('SELECT * FROM resources;')
+  .then(data => {
+    return data.rows;
+  });
+}
 
 
 
 
-module.exports = {  };
+
+module.exports = { getResources };
