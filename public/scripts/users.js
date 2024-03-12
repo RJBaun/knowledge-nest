6599a8e34c2f590d5bf6f5d3446fcf0f58f21e7f
v-lane <victoria.lane.m@gmail.com>
2024-03-12 12:52:08 -0400

    Add login page on login nav button click and add basic logout path

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,4 +1,12 @@
-// Client facing scripts here
+// Created by Victoria Lane
+// Created March 12, 2024
+// Purpose: Fetch user data and load user-related page sections.
+
+
+
+//////////////////////
+/// LISTENERS
+//////////////////////
 
 // On "Fetch Users" button click, all users are loaded
 $(() => {
@@ -20,21 +28,36 @@ $(() => {
 
 
 // On 'user-registration' nav button click, load registration page.
-$(() => {
-  $('#user-register').on('click', () => {
-    // COLLAPSE nav bar
-    // EMPTY all sections
-    $('#example-stuff').empty();
-    $(registrationPageMarkup()).appendTo('#section-registration-page');
-  });
+
+$('#user-register').on('click', () => {
+  // COLLAPSE nav bar
+  // EMPTY all sections
+  $('#example-stuff').empty();
+  $(registrationPageMarkup()).appendTo('#section-registration-page');
+});
+
+// On 'user-login' nav button click, load login page.
+$('#user-login').on('click', () => {
+  // COLLAPSE nav bar
+  // EMPTY all sections
+  $('#example-stuff').empty();
+  $(loginPageMarkup()).appendTo('#section-login-page');
+});
+
+// On 'user-logout' nav button click, logout user (clear cookies) and redirect to main page.
+$('#user-logout').on('click', () => {
+  // clear cookies
 });
 
 
 
 
 
+
+
+
 ///////////////////////////
-/// HTML Functions
+/// HTML FUNCTIONS
 ///////////////////////////
 
 /**
@@ -56,7 +79,27 @@ const registrationPageMarkup = () => {
     <label for="password" class="form-label">Password</label>
     <input type="password" class="form-control" id="password">
   </div>
-  <button id="register" type="submit" class="btn btn-primary">Register</button>
+  <button id="register-button" type="submit" class="btn btn-primary">Register</button>
 </form>`;
   return registrationPage;
+};
+
+/**
+ * Login page HTML
+ * @returns {string}
+ */
+const loginPageMarkup = () => {
+  const loginPage = `<form>
+  <h2> Login </h2>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password">
+  </div>
+  <button id="login-button" type="submit" class="btn btn-primary">Login</button>
+</form>`;
+  return loginPage;
 };
