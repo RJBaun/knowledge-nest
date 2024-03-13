0bc670d66b1cd136faf57e2ea60d00e3156f120e
Rylan Baun <rylanjbaun@gmail.com>
2024-03-13 09:57:47 -0600

    Add interacts-api router to server file, create route file

--- server.js
+++ server.js
@@ -38,12 +38,15 @@ app.use(methodOverride('_method'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const resourceApiRoutes = require('./routes/resources-api');
+const interactsApiRoutes = require('./routes/interacts-api');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/resources', resourceApiRoutes);
+app.use('/api/interacts', interactsApiRoutes);
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
